0831c8058e3eb7f7f86d93af53d9b97f6bf7e8f4
Marek Gierlicki <marek.gierlicki@netguru.com>
2022-09-13 21:12:14 +0200

    Authentication + Admin Dashboard Page

--- internal/render/render.go
+++ internal/render/render.go
@@ -26,6 +26,11 @@ func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateDa
 	td.Warning = app.Session.PopString(r.Context(), "warning")
 	td.Error = app.Session.PopString(r.Context(), "error")
 	td.CSRFToken = nosurf.Token(r)
+	if app.Session.Exists(r.Context(), "user_id") {
+		td.IsAuthenticated = 1
+	} else {
+		td.IsAuthenticated = 0
+	}
 	return td
 }
 
